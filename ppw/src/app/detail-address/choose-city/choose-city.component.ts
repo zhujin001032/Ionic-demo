1c4eec8f1ad311202ffbb8929dcfd186d2b90f61
何助金 <jasonhe@JasoniMac.local>
2019-04-16 15:40:46 +0800

    Revert "Revert "测试 input select""
    This reverts commit 7b2df9d72ba46e5899d71b828f339f257f4cb065.

--- ppw/src/app/detail-address/choose-city/choose-city.component.ts
+++ ppw/src/app/detail-address/choose-city/choose-city.component.ts
@@ -4,16 +4,18 @@ import { error } from '@angular/compiler/src/util';
 
 import { NavParams, ModalController } from '@ionic/angular';
 
+import { ChooseAddressComponent } from '../choose-address/choose-address.component';
 import { CityService } from './../../services/city.service';
 declare var BMap;
-declare var BMapLib; 
+declare var BMapLib;
 
 @Component({
   selector: 'app-choose-city',
   templateUrl: './choose-city.component.html',
   styleUrls: ['./choose-city.component.scss'],
 })
-export class ChooseCityComponent implements OnInit {
+export class ChooseCityComponent implements OnInit
+{
   // 城市数据
   /**
    * 当前定位城市
@@ -36,14 +38,22 @@ export class ChooseCityComponent implements OnInit {
   hotCityList: any;
   rowList: any;
 
-  constructor(public navParams: NavParams, private cityService: CityService, private modalCtrl: ModalController, ) {
+  constructor(
+    public navParams: NavParams,
+    private cityService: CityService,
+    private modalCtrl: ModalController,
+    public modalController: ModalController,
+  )
+  {
     this.hotCityList = [{ name: '深圳' }, { name: '西安' }, { name: '杭州' }, { name: '重庆' }, { name: '上海' }, { name: '北京' }];
     this.rowList = [];
     this.listdata = [];
     this.city = { name: '' };
   }
-  ngOnInit() {
-    if (this.cityService.getAddressData()) {
+  ngOnInit()
+  {
+    if (this.cityService.getAddressData())
+    {
       this.city.name = this.cityService.getAddressData().cityName;
     }
 
@@ -52,90 +62,127 @@ export class ChooseCityComponent implements OnInit {
   }
 
   //沟崽子们
-ionViewWillEnter(){
+  ionViewWillEnter()
+  {
     console.log('触发ionViewWillEnter');
-}
+  }
 
 
-ionViewWillLeave(){
+  ionViewWillLeave()
+  {
     console.log('触发ionViewWillLeave');
-}
+  }
 
-ionViewDidLeave(){
+  ionViewDidLeave()
+  {
     console.log('触发ionViewDidLeave');
-}
+  }
 
-ionViewWillUnload(){
+  ionViewWillUnload()
+  {
     console.log('触发ionViewWillUnload');
-}
+  }
 
-  ionViewDidLoad() { 
+  ionViewDidLoad()
+  {
     console.log('ionViewDidLoad');
-   
-}
 
-  ionViewDidEnter() {
+  }
+
+  ionViewDidEnter()
+  {
     console.log('ionViewDidEnter');
     let that = this;
-    this.cityService.getLocation().then((result:any)=>{
-      that.locationCity = result.name; 
+    this.cityService.getLocation().then((result: any) =>
+    {
+      that.locationCity = result.name;
       //使用localStoage存储cityName. 此处不可以使用this.localCityName = cityName; 因为这里的this 指向的是当前的类， 也就是 function(result)这个类
-       localStorage.setItem('currentCity', that.locationCity);
-       console.log('定位到当前城市',that.locationCity);
+      localStorage.setItem('currentCity', that.locationCity);
+      console.log('定位到当前城市', that.locationCity);
     })
-  //   let that = this;
-  //   var myCity = new BMap.LocalCity();
-  //   myCity.get(function (result) {
-  //      that.locationCity = result.name; 
-  //      //使用localStoage存储cityName. 此处不可以使用this.localCityName = cityName; 因为这里的this 指向的是当前的类， 也就是 function(result)这个类
-  //       localStorage.setItem('currentCity', that.locationCity);
-  //       console.log('定位到当前城市',that.locationCity);
-  //       // alert('定位到当前城市'+ that.locationCity);
-  //       return that.locationCity;
-  //  });
+    //   let that = this;
+    //   var myCity = new BMap.LocalCity();
+    //   myCity.get(function (result) {
+    //      that.locationCity = result.name; 
+    //      //使用localStoage存储cityName. 此处不可以使用this.localCityName = cityName; 因为这里的this 指向的是当前的类， 也就是 function(result)这个类
+    //       localStorage.setItem('currentCity', that.locationCity);
+    //       console.log('定位到当前城市',that.locationCity);
+    //       // alert('定位到当前城市'+ that.locationCity);
+    //       return that.locationCity;
+    //  });
 
-   //延迟500毫秒取存储在localStorage中的 cityName 
-   setTimeout(() => {
-       this.locationCity = localStorage.getItem('currentCity'); 
-  }, 500); 
+    //延迟500毫秒取存储在localStorage中的 cityName 
+    setTimeout(() =>
+    {
+      this.locationCity = localStorage.getItem('currentCity');
+    }, 500);
   }
 
   // 热门城市
-  getRowListByGridList(size) {
-    if (this.rowList.length > 1) {
+  getRowListByGridList(size)
+  {
+    if (this.rowList.length > 1)
+    {
       return this.rowList;
     }
-    for (let i = 0; i < this.hotCityList.length; i += size) {
+    for (let i = 0; i < this.hotCityList.length; i += size)
+    {
       const listTemp = [];
       listTemp.push(this.hotCityList.slice(i, i + size));
       this.rowList.push(listTemp);
     }
     return this.rowList;
   }
-  getCityData() {
-    this.cityService.getLocalCityData().subscribe(res => {
+  getCityData()
+  {
+    this.cityService.getLocalCityData().subscribe(res =>
+    {
       const tempList = res.json();
       // let tempList: Array = Array(tempObj._body);
-      for (let i = 0; i < tempList.length; i++) {
-        if (tempList[i].code > 999 && tempList[i].code < 10000) {
+      for (let i = 0; i < tempList.length; i++)
+      {
+        if (tempList[i].code > 999 && tempList[i].code < 10000)
+        {
           this.listdata.push(tempList[i]);
         }
       }
 
       console.log('cityData----------------->>', this.listdata);
       // tslint:disable-next-line:no-shadowed-variable
-    }, error => {
-      console.log(error);
-    }
+    }, error =>
+      {
+        console.log(error);
+      }
     );
   }
-  backWithCity() {
+  backWithCity()
+  {
     this.navParams.data.modal.dismiss();
   }
 
-  onCurrentCityClick(city: any) {
+  onCurrentCityClick(city: any)
+  {
     console.log('当前选择的城市' + city.name);
     // 返回参数
     this.modalCtrl.dismiss({ 'res': city });
+    // this.chooseCity(); // 测试 多层 模态
+  }
+
+  async chooseCity()
+  {
+    console.log('选择城市：');
+    const modal = await this.modalController.create({
+      component: ChooseAddressComponent,
+      // componentProps: { value: this.addressData.city }
+    });
+    await modal.present();
+    // 模态返回 数据
+    const { data } = await modal.onDidDismiss();
+    console.log(data);
+    if (data)
+    {
+      // this.addressData.cityName = data['res'].name;
+    }
+
   }
 }
--- ppw/src/app/tab2/tab2.page.ts
+++ ppw/src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CallNumber } from '@ionic-native/call-number/ngx';
+
 import { Picker } from 'ng-zorro-antd-mobile';
 
 @Component({
@@ -7,7 +11,8 @@ import { Picker } from 'ng-zorro-antd-mobile';
   styleUrls: ['tab2.page.scss'],
   providers: [Picker],
 })
-export class Tab2Page {
+export class Tab2Page
+{
   public singleArea = ["hhh", "hksahjks", "gjasgh"];
   // [{ label: '深圳市', code: '4403', children: [{ label: '南山区', code: '440301' }, { label: '南油', code: '440302' }] },
   // { label: '北京市', code: '11', children: [{ label: '海定区', code: '1101' }, { label: '崇文区', code: '1102' }] }];
@@ -17,34 +22,82 @@ export class Tab2Page {
   name1 = '请选择';
   value = [];
   name = '请选择';
-  onOk1(result) {
+
+  users: any[] = [
+    {
+      id: 1,
+      first: 'HE',
+      last: 'Jason',
+    },
+    {
+      id: 2,
+      first: 'HE',
+      last: 'King',
+    },
+    {
+      id: 3,
+      first: 'HE',
+      last: 'jin',
+    }
+  ]
+
+
+  compareWithFn = (o1, o2) =>
+  {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
+
+  compareWith = this.compareWithFn;
+
+  constructor(private callNumber: CallNumber)
+  {
+
+  }
+  ngOnInit()
+  {
+    const formbuilder = new FormBuilder();
+
+  }
+  onOk1(result)
+  {
     this.name1 = this.getResult(result);
     console.log('----------------' + this.name1);
   }
-  onDismiss1() {
+  onDismiss1()
+  {
     console.log('cancel');
   }
-  getResult(result) {
+  getResult(result)
+  {
     this.value = [];
     let temp = '';
-    result.forEach(item => {
+    result.forEach(item =>
+    {
       this.value.push(item.label || item);
       temp += item.label || item;
     });
     return this.value.map(v => v).join(',');
   }
 
-
-  getValue(result) {
+  callPhone()
+  {
+    this.callNumber.callNumber('18001010101', true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
+  }
+  getValue(result)
+  {
     const value = [];
     let temp = '';
-    result.forEach(item => {
+    result.forEach(item =>
+    {
       value.push(item.value || item);
       temp += item.value || item;
     });
     return value;
   }
-  detailAddress() {
+  detailAddress()
+  {
     console.log('click detail address button 1');
     // this.nav.navigateForward("/detail-address");
   }
